e70c6933ab4ba2d95520aab920df3ff5a712bf7a
huangxf <huangxiangfeng@3ikids.com>
2015-06-25 18:55:33 +0800

    Copy_2_OrderByObjectBean.java

---
+++ src/com/kbtc/utils/Copy_2_of_OrderByObjectBean.java
@@ -0,0 +1,188 @@
+package com.kbtc.utils;
+
+public class Copy_2_of_OrderByObjectBean {
+
+	public Object a;
+	public Object b;
+	public Object c;
+	public Object d;
+	public Object e;
+	public Object f;
+	public Object g;
+	public Object h;
+	public Object i;
+	public Object j;
+	public Object k;
+	public Object l;
+	public Object m;
+	public Object n;
+	public Object o;
+	public Object p;
+	public Object q;
+	public Object r;
+	public Object s;
+	public Object t;
+	public Object u;
+	public Object v;
+	public Object w;
+	public Object x;
+	public Object y;
+	public Object z;
+	public Object getA() {
+		return a;
+	}
+	public void setA(Object a) {
+		this.a = a;
+	}
+	public Object getB() {
+		return b;
+	}
+	public void setB(Object b) {
+		this.b = b;
+	}
+	public Object getC() {
+		return c;
+	}
+	public void setC(Object c) {
+		this.c = c;
+	}
+	public Object getD() {
+		return d;
+	}
+	public void setD(Object d) {
+		this.d = d;
+	}
+	public Object getE() {
+		return e;
+	}
+	public void setE(Object e) {
+		this.e = e;
+	}
+	public Object getF() {
+		return f;
+	}
+	public void setF(Object f) {
+		this.f = f;
+	}
+	public Object getG() {
+		return g;
+	}
+	public void setG(Object g) {
+		this.g = g;
+	}
+	public Object getH() {
+		return h;
+	}
+	public void setH(Object h) {
+		this.h = h;
+	}
+	public Object getI() {
+		return i;
+	}
+	public void setI(Object i) {
+		this.i = i;
+	}
+	public Object getJ() {
+		return j;
+	}
+	public void setJ(Object j) {
+		this.j = j;
+	}
+	public Object getK() {
+		return k;
+	}
+	public void setK(Object k) {
+		this.k = k;
+	}
+	public Object getL() {
+		return l;
+	}
+	public void setL(Object l) {
+		this.l = l;
+	}
+	public Object getM() {
+		return m;
+	}
+	public void setM(Object m) {
+		this.m = m;
+	}
+	public Object getN() {
+		return n;
+	}
+	public void setN(Object n) {
+		this.n = n;
+	}
+	public Object getO() {
+		return o;
+	}
+	public void setO(Object o) {
+		this.o = o;
+	}
+	public Object getP() {
+		return p;
+	}
+	public void setP(Object p) {
+		this.p = p;
+	}
+	public Object getQ() {
+		return q;
+	}
+	public void setQ(Object q) {
+		this.q = q;
+	}
+	public Object getR() {
+		return r;
+	}
+	public void setR(Object r) {
+		this.r = r;
+	}
+	public Object getS() {
+		return s;
+	}
+	public void setS(Object s) {
+		this.s = s;
+	}
+	public Object getT() {
+		return t;
+	}
+	public void setT(Object t) {
+		this.t = t;
+	}
+	public Object getU() {
+		return u;
+	}
+	public void setU(Object u) {
+		this.u = u;
+	}
+	public Object getV() {
+		return v;
+	}
+	public void setV(Object v) {
+		this.v = v;
+	}
+	public Object getW() {
+		return w;
+	}
+	public void setW(Object w) {
+		this.w = w;
+	}
+	public Object getX() {
+		return x;
+	}
+	public void setX(Object x) {
+		this.x = x;
+	}
+	public Object getY() {
+		return y;
+	}
+	public void setY(Object y) {
+		this.y = y;
+	}
+	public Object getZ() {
+		return z;
+	}
+	public void setZ(Object z) {
+		this.z = z;
+	}
+	
+}
